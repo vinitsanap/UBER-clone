8ef5d6af064970b93da95c5a3344982bf45f18d4
Vinit Sanap <vinitwork20@gmail.com>
2026-07-26 16:03:52 +0530

    captain authentication done

--- backend/controller/captain.controller.js
+++ backend/controller/captain.controller.js
@@ -1,9 +1,10 @@
 const captainModel = require('../models/captain.model');
 const captainService = require('../service/captain.service');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const { validationResult } = require('express-validator');
 
 
-module.exports.registerCaptain = async (req, res) => {
+module.exports.registerCaptain = async (req, res, next) => {
 
     const error = validationResult(req);
     if (!error.isEmpty()) {
@@ -35,4 +36,44 @@ module.exports.registerCaptain = async (req, res) => {
 
     res.status(201).json({ token, captain });
 
+}
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    
+    const captain = await captainModel.findOne({ email }).select('+password');
+
+    if (!captain) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await captain.comparePassword(password);
+    
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = captain.generateAuthToken();
+
+    res.cookie('token', token);
+    
+    res.status(200).json({ token, captain });
+}
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json(req.captain);
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+await blacklistTokenModel.create({ token: token }); 
+
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logout successful' });
 }
--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const captainController = require('../controller/captain.controller');
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+const authMiddleware = require('../Middlewares/auth.middleware');   
 
 router.post('/register', [
     body('fullname.firstname').isLength({ min: 3 }).withMessage('Firstname must be at least 3 characters long'),
@@ -15,5 +16,14 @@ router.post('/register', [
     captainController.registerCaptain   
 )
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email address'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], 
+    captainController.loginCaptain);
+
+router.get('/profile', authMiddleware.authCaptain,  captainController.getCaptainProfile);
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain);
 
 module.exports = router;
